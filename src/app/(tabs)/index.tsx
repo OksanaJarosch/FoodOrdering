9577387d521e7d3eb49ddd16e044c0e89da9b897
Oksana Jarosch <ok.jarosch@gmail.com>
2024-08-09 15:34:22 +0200

    created list of items and product item

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,21 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, View } from 'react-native';
+import products from '@/assets/data/products';
+import { ProductListItem } from '@/src/components/ProductListItem';
 
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View>
+      <FlatList
+        data={products}
+        renderItem={({item}) =>
+          <ProductListItem product={item}/>
+        }
+        keyExtractor={item => item.id.toString()}
+        numColumns={2}
+        contentContainerStyle={{gap: 10, padding: 10}}
+        columnWrapperStyle={{gap: 10}}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,46 @@
+import Colors from '@/src/constants/Colors';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { Product } from '@/src/types';
+
+const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+type ProductListItemProps = {
+    product: Product;
+}
+
+export const ProductListItem = ({product}: ProductListItemProps) => {
+    return (
+        <View style={styles.container}>
+            <Image
+                source={{uri: product.image || defaultPizzaImage}}
+                style={styles.image}
+                resizeMode='contain'
+            />
+            <Text style={styles.title}>{product.name}</Text>
+            <Text style={styles.price}>${product.price}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        maxWidth: '50%',
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 20,
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 1,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: '600',
+        marginVertical: 10,
+    },
+    price: {
+        color: Colors.light.tint,
+        fontWeight: 'bold',
+    },
+});
